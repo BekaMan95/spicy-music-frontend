ecd74dac20368eef9dbc9857027be0549100cf90
BekaMan95 <bsahle95@gmail.com>
2025-10-19 23:05:34 +0300

    ShadCN, Router, Toolkit and Saga setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { ThemeProvider } from "@/components/theme-provider"
+import './input.css'
+import { ModeToggle } from './components/mode-toggle'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Router>
+        <Routes>
+          <ModeToggle />
+        </Routes>
+      </Router>
+    </ThemeProvider>
   </StrictMode>,
 )
