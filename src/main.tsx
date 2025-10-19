6b136a37dfabea3f012dc4587999919055430372
BekaMan95 <bsahle95@gmail.com>
2025-10-20 00:12:29 +0300

    shadCN removed

--- src/main.tsx
+++ src/main.tsx
@@ -1,20 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { ThemeProvider } from "@/components/theme-provider"
 import './input.css'
-import { ModeToggle } from './components/mode-toggle'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 
 
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
       <Router>
         <Routes>
-          <ModeToggle />
+          <h1> Hello World!!!! </h1>
         </Routes>
       </Router>
-    </ThemeProvider>
   </StrictMode>,
 )
